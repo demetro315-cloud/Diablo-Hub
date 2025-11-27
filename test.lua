7a01077d09f7574b76634e8715052a3d28c4cf79
demetro315-cloud <demetro315@gmail.com>
2025-11-26 22:19:05 -0500

    Delete test.lua

--- test.lua
+++
@@ -1,8 +0,0 @@
-return function()
-    print("GitHub script loaded successfully!")
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Diablo Hub",
-        Text = "Loaded successfully!",
-        Duration = 5
-    })
-end
